e95749bf985527161f9f9edcc8ee61badde136cd
zhafir <zzzhafir75@gmail.com>
2020-06-22 16:22:18 +0800

    Add test.cpp, .gitignore, VSCode build task

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class TestObject
+{
+private:
+    int a, b;
+public:
+    TestObject(int a, int b);
+    double Divide();
+};
+
+TestObject::TestObject(int a1, int b1)
+{
+    a = a1;
+    b = b1;
+
+    std::cout << "TestObject : " << a + b << std::endl;
+}
+
+double TestObject::Divide(){
+    return (double) a / b;
+}
+
+int main(){
+    int a,b;
+
+    std::cout << "Enter a : ";
+    std::cin >> a;
+    std::cout << "Enter b : ";
+    std::cin >> b;
+    
+    TestObject t(a, b);
+    double d = t.Divide();
+
+    std::cout << "Divide result : " << d << std::endl;
+}
